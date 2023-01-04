df62d276c1b10eb13928225da78e8a6476de20cf
andreiggr <andrei.ggr@gmail.com>
2023-01-04 14:56:37 +0200

    upgrade step

---
+++ eea/api/dataconnector/upgrades/to4.py
@@ -0,0 +1,14 @@
+
+''' upgrade to 4.0 '''
+
+
+def run_upgrade(setup_context):
+    """ run upgrade to 4.0
+    """
+
+    setup_context.runImportStepFromProfile(
+        "profile-eea.api.dataconnector:default",
+        "typeinfo",
+        run_dependencies=False,
+        purge_old=False,
+    )
