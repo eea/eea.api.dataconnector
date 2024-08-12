b84e816329ead39355e00acd59ed40db2c1929b0
Tripon Eugen <tripon.eugen@gmail.com>
2024-08-12 17:04:14 +0300

    Refs #273385 - plone read zip using with

--- eea/api/dataconnector/subscriber.py
+++ eea/api/dataconnector/subscriber.py
@@ -2,20 +2,14 @@
 
 from io import BytesIO
 from zipfile import ZipFile
-import logging
 
 from persistent.mapping import PersistentMapping
 from plone.namedfile.file import NamedBlobFile
 from zope.annotation.interfaces import IAnnotations
 
 
-logger = logging.getLogger(__name__)
-
-
 def annotations_flourish_set(obj, event):
     """ annotations_flourish_set """
-    # import pdb
-    # pdb.set_trace()
     annotations = IAnnotations(obj)
     annot_data = PersistentMapping()
     blob = obj.flourish_zip
@@ -23,13 +17,10 @@ def annotations_flourish_set(obj, event):
         blob_content = blob.data
 
         annot_data = PersistentMapping()
-        with ZipFile(BytesIO(blob_content), mode="r") as myzip:
+        with ZipFile(BytesIO(blob_content)) as myzip:
             for file_name in myzip.namelist():
-                file_content = myzip.open(file_name).read()
-                file = NamedBlobFile(filename=file_name, data=file_content)
-                annot_data[file_name] = file
+                with myzip.open(file_name) as file_content:
+                    file = NamedBlobFile(
+                        filename=file_name, data=file_content.read())
+                    annot_data[file_name] = file
     annotations['flourish_zip'] = annot_data
-
-    # import pdb
-    # pdb.set_trace()
-    logger.warning("FlourishObjectModified")
