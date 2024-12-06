b173f36d9d30053d42315440b45335841005cd2c
Miu Razvan <miu.razvan28@gmail.com>
2024-12-06 15:38:01 +0200

    update

--- eea/api/dataconnector/browser/deserializer/dxfields.py
+++ eea/api/dataconnector/browser/deserializer/dxfields.py
@@ -24,43 +24,6 @@ def getLink(path):
     return path
 
 
-def getUid(context, link, retry=True):
-    """
-      Get the UID corresponding to a given link.
-
-      Parameters:
-      - context: The context or object providing the link.
-      - link (str): The link for which to retrieve the UID.
-      - retry (bool, optional): If True, attempt to resolve the UID
-        even if the initial attempt fails. Defaults to True.
-
-      Returns:
-      - str or None: The UID corresponding to the provided link,
-        or None if the link is empty or cannot be resolved.
-
-      If the link is empty, the function returns the link itself.
-      If the link cannot be resolved in the initial attempt and retry
-      is True, the function retries resolving the link by calling itself
-      with retry set to False.
-
-      The function uses the RESOLVEUID_RE regular expression to match
-      and extract the UID from the link.
-      """
-
-    if not link:
-        return link
-    match = RESOLVEUID_RE.match(link)
-    if match is None:
-        if not retry:
-            return link
-        # Alin Voinea a zis sa las asa
-        return getUid(context, path2uid(context=context, link=getLink(link)),
-                      False)
-
-    uid, _ = match.groups()
-    return uid
-
-
 @implementer(IFieldDeserializer)
 @adapter(IJSONField, IDexterityContent, IBrowserRequest)
 class JSONFieldDeserializer(DefaultFieldDeserializer):
@@ -69,7 +32,6 @@ class JSONFieldDeserializer(DefaultFieldDeserializer):
     def __call__(self, value):
         if isinstance(value, dict) and 'provider_url' in value:
             url = value["provider_url"]
-            uid = getUid(self.context, url)
-            value["provider_url"] = ("../resolveuid/%s" %
-                                     uid) if uid != url else url
+            resolveuid = path2uid(context=self.context, link=getLink(url))
+            value["provider_url"] = resolveuid if resolveuid != url else url
         return value
