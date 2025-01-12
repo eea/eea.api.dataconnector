5fc71c652796ac6080951aea49c9ef0be929373b
dobri1408 <dobriceanionut1408@gmail.com>
2025-01-12 14:41:13 +0200

    Add upgrade step for resolve uid generation

--- eea/api/dataconnector/upgrades/upgrade_visualizations.py
+++ eea/api/dataconnector/upgrades/upgrade_visualizations.py
@@ -6,20 +6,22 @@ from urllib.parse import urlparse
 from plone.uuid.interfaces import IUUID
 from plone.restapi.deserializer.utils import path2uid
 
+
 def getLink(path):
     """
-      Get link
-      """
-
+    Get link from a given path.
+    """
     URL = urlparse(path)
 
     if URL.netloc.startswith('localhost') and URL.scheme:
-        return path.replace(URL.scheme + "://" + URL.netloc, "")
+        return path.replace(f"{URL.scheme}://{URL.netloc}", "")
     return path
 
-def upgrade_visualizations(portal):
-    """Upgrade step to handle provider_url in visualizations"""
 
+def upgrade_visualizations(portal):
+    """
+    Upgrade step to handle provider_url in visualizations.
+    """
     i = 0
     brains = portal.portal_catalog(portal_type="viz")
     pghandler = ZLogHandler(100)
@@ -29,7 +31,7 @@ def upgrade_visualizations(portal):
         pghandler.report(idx)
         try:
             obj = brain.getObject()
-        except Exception as e:
+        except Exception:
             continue  # Skip objects that cannot be retrieved
 
         viz = getattr(obj, 'visualization', None)
@@ -39,17 +41,15 @@ def upgrade_visualizations(portal):
 
         provider_url = viz.get('provider_url', None)
 
-        if provider_url and 'resolve_uid' not in provider_url: 
-            uuid =path2uid(
-            context=obj, link=getLink(provider_url))
-            viz['provider_url'] = uuid;
-            obj.visualization=viz;
+        if provider_url and 'resolve_uid' not in provider_url:
+            uuid = path2uid(context=obj, link=getLink(provider_url))
+            viz['provider_url'] = uuid
+            obj.visualization = viz
             modified(obj)
-            i=i+1;
+            i += 1
 
-        if not i % 100:
+        if i % 100 == 0:
             transaction.commit()
 
     pghandler.finish()
     transaction.commit()
-
