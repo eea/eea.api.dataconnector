3ff5b3d7a3fffa1586b80486965730ce8e6c646e
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2025-01-31 16:00:55 +0200

    Disable transform for file serving

--- eea/api/dataconnector/browser/flourish.py
+++ eea/api/dataconnector/browser/flourish.py
@@ -1,7 +1,6 @@
-""" flourish
-"""
+"""flourish"""
 
-from zipfile import (ZipFile, is_zipfile)
+from zipfile import ZipFile, is_zipfile
 
 from plone.namedfile.utils import set_headers, stream_data
 from plone.namedfile.file import NamedBlobFile
@@ -21,13 +20,13 @@ class FlourishUpload(BrowserView):
     """Upload a zip file, extract it and add to context annotation"""
 
     def list(self):
-        """ upload form and show current flourish from annotation """
+        """upload form and show current flourish from annotation"""
         response = {}
-        response['title'] = self.context.title
-        response['portal_type'] = self.context.portal_type
-        response['url'] = self.context.absolute_url()
-        response['annotations'] = []
-        response['message'] = None
+        response["title"] = self.context.title
+        response["portal_type"] = self.context.portal_type
+        response["url"] = self.context.absolute_url()
+        response["annotations"] = []
+        response["message"] = None
         annotations = IAnnotations(self.context)
         fileUploaded = self.request.form.get("fileToUpload", None)
         if fileUploaded:
@@ -37,19 +36,21 @@ class FlourishUpload(BrowserView):
                     for file_name in myzip.namelist():
                         with myzip.open(file_name) as file_content:
                             file = NamedBlobFile(
-                                filename=file_name, data=file_content.read())
+                                filename=file_name, data=file_content.read()
+                            )
                             annot_data[file_name] = file
-                annotations['flourish_zip'] = annot_data
+                annotations["flourish_zip"] = annot_data
             else:
-                response['message'] = 'The file sent was either empty \
-                     or not a valid zip archive.'
+                response["message"] = "The file sent was either empty \
+                     or not a valid zip archive."
         elif self.request.form.get("delete", None):
-            annotations['flourish_zip'] = []
+            annotations["flourish_zip"] = []
 
-        if 'flourish_zip' in annotations:
-            response['annotations'] = annotations['flourish_zip']
+        if "flourish_zip" in annotations:
+            response["annotations"] = annotations["flourish_zip"]
         return response
 
+from plone.transformchain.interfaces import DISABLE_TRANSFORM_REQUEST_KEY
 
 @implementer(IPublishTraverse)
 class FlourishDownload(BrowserView):
@@ -60,7 +61,7 @@ class FlourishDownload(BrowserView):
         self.filename = None
 
     def publishTraverse(self, request, name):
-        """ traverse """
+        """traverse"""
         if self.filename is None:  # ../@@download/fieldname/filename
             self.filename = name
         else:
@@ -68,14 +69,16 @@ class FlourishDownload(BrowserView):
         return self
 
     def __call__(self):
-        self.request.response.setHeader('X-Theme-Disabled', '1')
+        self.request.response.setHeader("X-Theme-Disabled", "1")
         file = self._getFile()
-        self.set_headers(file)
+
+        set_headers(file, self.request.response)
         request_range = self.handle_request_range(file)
+        self.request.environ[DISABLE_TRANSFORM_REQUEST_KEY] = True
         return stream_data(file, **request_range)
 
     def handle_request_range(self, file):
-        """ handle_request_range """
+        """handle_request_range"""
         default = {}
         # check if we have a range in the request
         header_range = self.request.getHeader("Range", None)
@@ -103,13 +106,8 @@ class FlourishDownload(BrowserView):
         except ValueError:
             return default
 
-    def set_headers(self, file):
-        """ set_headers """
-
-        set_headers(file, self.request.response)
-
     def _getFile(self):
-        """ _getFiles """
+        """_getFiles"""
 
         context = getattr(self.context, "aq_explicit", self.context)
         annotations = IAnnotations(context)
