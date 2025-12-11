153b00cde9ee0282bde91f5310845fdcf4ab35bf
laszlocseh <csehlaszlo91@gmail.com>
2025-12-11 14:08:14 +0200

    fix: refs #295504 not equal formatting error

--- eea/api/dataconnector/queryparser.py
+++ eea/api/dataconnector/queryparser.py
@@ -151,7 +151,8 @@ def getDataQuery(form):
     data = []
     for expression in form:
         value = form.get(expression)
-        op = re.search(r"\b(gt|gte|lt|lte|eq|ne|in|nin|like|not_like)\b", expression)
+        op = re.search(
+            r"\b(gt|gte|lt|lte|eq|ne|in|nin|like|not_like)\b", expression)
         dataType = re.search(r"\b(int|float)\b", expression)
         index = re.sub(
             r"(\[(gt|gte|lt|lte|eq|ne|in|nin|like|not_like)\])|(\:int|float)",
